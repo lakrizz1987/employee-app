37f5398e95caef55aa27de8bedce75ff8953a7dd
Ivaylo <ignatov.ivaylo@gmail.com>
2023-03-10 11:42:42 +0200

    fetching tasks

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,16 +10,20 @@ const Home = () => {
     useEffect(() => {
         async function getAllEmployees() {
             const employees = await api.getAll();
-            
+            const tasks = await api.getAllTasks();
+
             const data = [];
             for (const key in employees) {
+                const searchedTask = Object.values(tasks).filter(task => task.userId === key && task.completed === false)[0];
+                console.log(searchedTask)
                 data.push({
                     id: key,
                     birthDate: employees[key].birthDate,
                     email: employees[key].email,
                     name: employees[key].name,
                     phone: employees[key].phone,
-                    salary: employees[key].salary
+                    salary: employees[key].salary,
+                    task: searchedTask
                 })
             }
 
@@ -46,7 +50,18 @@ const Home = () => {
                                 <td>
                                     <Link to={`/employee/${employee.id}`}>{employee.name}</Link>
                                 </td>
-                                <td>Francisco Chang</td>
+                                <td>{employee.task ?
+                                    <>
+                                        <p>{employee.task.title}</p>
+                                        <Link to={`/tasks/${employee.task.taskId}`}>View more</Link>
+                                    </>
+                                    :
+                                    <>
+                                        <p>No tasks</p>
+                                        <Link>Add task</Link>
+                                    </>
+                                }</td>
+
                             </tr>
                         )
                     })}
--- src/service/api.js
+++ src/service/api.js
@@ -14,6 +14,21 @@ const getAll = async () => {
     }
 };
 
+const getAllTasks = async () => {
+    try {
+        const res = await fetch(`${baseUrl}/tasks.json`);
+        if (!res.ok) {
+            const err = await res.json();
+            throw new Error({ message: err.message })
+        }
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+
 const getOneById = async (id) => {
     try {
         const res = await fetch(`${baseUrl}/users/${id}.json`);
@@ -32,6 +47,7 @@ const getOneById = async (id) => {
 const api = {
     getAll,
     getOneById,
+    getAllTasks
 }
 
 export default api;
